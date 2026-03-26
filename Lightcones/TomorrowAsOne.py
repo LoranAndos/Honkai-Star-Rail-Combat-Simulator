a4626e3c9e30f9a7cfeffcf9a28cb700d71db47f
LoranAndos <rapetje@gmail.com>
2026-03-26 03:20:37 +0100

    Wording Changes
    As said above.

--- Lightcones/TomorrowAsOne.py
+++ Lightcones/TomorrowAsOne.py
@@ -2,7 +2,7 @@ from Buff import *
 from Lightcone import Lightcone
 from Attributes import *
 
-class TomorrowWithUsAll(Lightcone):
+class TomorrowAsOne(Lightcone):
     name = "Tomorrow, With Us All"
     path = Path.ELATION
     baseHP = 953
@@ -15,11 +15,11 @@ class TomorrowWithUsAll(Lightcone):
     def equip(self):
         bl, dl, al, dl, hl = super().equip()
         BuffAmount = self.level * 0.03 + 0.09
-        bl.append(Buff("TomorrowWithUsCD", StatTypes.CD_PERCENT, BuffAmount, self.wearerRole, [AtkType.ALL], 1, 1, Role.SELF, TickDown.PERM))
+        bl.append(Buff("TomorrowAsOneCD", StatTypes.CD_PERCENT, BuffAmount, self.wearerRole, [AtkType.ALL], 1, 1, Role.SELF, TickDown.PERM))
         return bl, dl, al, dl, hl
 
     def useUlt(self, enemyID=-1):
         bl, dl, al, dl, hl = super().useUlt()
         elaAmount = self.level * 0.01 + 0.07
-        bl.append(Buff("ForeverVictual_UltELA", StatTypes.ELA, elaAmount, Role.ALL, [AtkType.ALL], 1, 1, Role.SELF, TickDown.END))
+        bl.append(Buff("TomrrowAsOneUltELA", StatTypes.ELA, elaAmount, Role.ALL, [AtkType.ALL], 1, 1, Role.SELF, TickDown.END))
         return bl, dl, al, dl, hl
